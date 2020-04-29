4c883fa12346c525768cacb217d799d67c29ff98
enrico-kaack <e.kaack@live.de>
2020-04-29 17:17:34 +0200

    Clean up

--- lib/main.dart
+++ lib/main.dart
@@ -23,24 +23,14 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Translator'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({Key key}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -48,42 +38,35 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final ValueNotifier<List<TranslationEntry>> newTranslationNotifier = new ValueNotifier([]);
+  var db = TranslationService();
+
+
 
   void _updateList(List<TranslationEntry> translations) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       newTranslationNotifier.value = translations;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Translator"),
       ),
       body: Column(
         children: <Widget>[
-          TextField(
-            onChanged: (String value) async {
-              var db = TranslationService();
-              await db.openDatabaseOrCopyIfNotExists();
-              var result = await db.search(value);
-              _updateList(result);
-            },
+          Container(
+            margin: EdgeInsets.all(5.0),
+            child: TextField(
+              onChanged: (String value) async {
+                await db.openDatabaseOrCopyIfNotExists();
+                var result = await db.search(value);
+                _updateList(result);
+              },
+            ),
           ),
+          Divider(),
           Expanded(child: ResultList(newTranslationNotifier))
         ],
       ),
