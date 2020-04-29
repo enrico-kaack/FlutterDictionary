dc4a5b2476931d227d40fa859b4ef48d8ab9ee5e
enrico-kaack <e.kaack@live.de>
2020-04-29 17:13:11 +0200

    Added: seperated detail screen

--- lib/main.dart
+++ lib/main.dart
@@ -100,21 +100,84 @@ class ResultList extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    return ListView.separated(
       itemCount: this.translationListener.value.length,
       itemBuilder: (context, index)
       {
         return ListTile(
-          title: Text(
-              this.translationListener.value[index].source_representation
-          ),
-          subtitle: Text(
-              this.translationListener.value[index].bestTranslation().translation
-          ),
+          title: Text(this.translationListener.value[index].source_representation),
+          subtitle: Text(this.translationListener.value[index].bestTranslation().translation),
+          trailing: Icon(Icons.keyboard_arrow_right),
+          onTap: (){
+            Navigator.push(
+              context,
+              new MaterialPageRoute(builder: (ctxt) => new SecondScreen(this.translationListener.value[index])),
+            );
+          },
         );
-      }
+      },
+      separatorBuilder: (context, index) => Divider(),
     );}
 
 }
 
+_renderVariations(List<TranslationSenseEntry> variations){
+  return Column(
+    children: variations.map((e) =>
+      Row(
+        children: <Widget>[
+          Expanded(child: Text(e.sense)),
+          Expanded(child: Text(e.translation)),
+          Divider(
+            thickness: 1,
+          )
+        ],
+      )
+    ).toList(),
+  );
+}
 
+
+class SecondScreen extends StatefulWidget {
+  TranslationEntry translationEntry;
+
+
+  SecondScreen(TranslationEntry translationEntry){
+    this.translationEntry = translationEntry;
+  }
+
+
+
+  @override
+  State<StatefulWidget> createState()=> _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen>{
+
+  @override
+  Widget build (BuildContext ctxt) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.translationEntry.source_representation),
+      ),
+      body: new Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+                itemCount: widget.translationEntry.variations.length,
+                itemBuilder: (context, index){
+                  return Card(
+                    child: ListTile(
+                      title: Text(widget.translationEntry.variations[index].translation),
+                      subtitle: Text(widget.translationEntry.variations[index].sense),
+                    ),
+                    margin: EdgeInsets.symmetric(vertical: 2.0, horizontal: 2.0),
+                  );
+                },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
