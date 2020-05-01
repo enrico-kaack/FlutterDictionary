0cb3ce7cf48c48efc34a1b56e94ebf8a65bc5321
enrico-kaack <e.kaack@live.de>
2020-05-01 17:55:39 +0200

    Fixed: org name and app name for desktop

--- go/cmd/options.go
+++ go/cmd/options.go
@@ -6,15 +6,18 @@ import (
 	"github.com/go-flutter-desktop/go-flutter"
 )
 
+var orgName = "de.ek"
+var appName = "offlineDictionary"
+
 var options = []flutter.Option{
 	flutter.WindowInitialDimensions(800, 1280),
-	flutter.AddPlugin(sqflite.NewSqflitePlugin("myOrganizationOrUsername","myApplicationName")),
+	flutter.AddPlugin(sqflite.NewSqflitePlugin(orgName,appName)),
 	flutter.AddPlugin(&path_provider.PathProviderPlugin{
-    	VendorName:      "myOrganizationOrUsername",
-    	ApplicationName: "myApplicationName",
+    	VendorName:      orgName,
+    	ApplicationName: appName,
     }),
     flutter.AddPlugin(&shared_preferences.SharedPreferencesPlugin{
-    	VendorName:      "myOrganizationOrUsername",
-    	ApplicationName: "myApplicationName",
+    	VendorName:      orgName,
+    	ApplicationName: appName,
     }),
 }
